6eb0ad9492f8436afa9e332459f43138c55bd7d0
accuracy-eminent <145879231+accuracy-eminent@users.noreply.github.com>
2026-04-25 20:59:52 -0500

    Add initial BIOS

---
+++ src/bios.c
@@ -0,0 +1,86 @@
+// Todo
+
+void boot()
+{
+
+}
+
+void wboot()
+{
+
+}
+
+int con_st()
+{
+
+}
+
+char conin()
+{
+
+}
+
+void conout(char c)
+{
+
+}
+
+void list(char c)
+{
+
+}
+
+void punch(char c)
+{
+
+}
+
+char reader()
+{
+
+}
+
+void home()
+{
+
+}
+
+char* seldsk(char disk)
+{
+
+}
+
+void settrk(int trk)
+{
+
+}
+
+void setsec(char sec)
+{
+
+}
+
+void setdma(char *addr)
+{
+
+}
+
+char read_s()
+{
+
+}
+
+char write_s(char deblocking)
+{
+    
+}
+
+char listst()
+{
+
+}
+
+char sectran(int log_sector, int *tran_table)
+{
+
+}
